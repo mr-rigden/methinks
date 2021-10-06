b71eeb72453af4281041c3f193311f8cc8aafebf
Jason Rigden <jasonrigden@gmail.com>
2021-10-06 10:23:43 -0700

    copyright

--- script.js
+++ script.js
@@ -12,8 +12,35 @@ try {
     var shareURL = window.location.href
 }
 
+var shareButton = document.getElementById("share");
+
+function newShare() {
+
+    var cta = document.getElementById("share")
+    cta.innerHTML = 'Share';
+    forwardButton.onclick = function () {
+  
+      navigator.share({
+        title: document.getElementById("page-title").innerText,
+        url: shareURL,
+      })
+    }
+  }
+
+function twitterShare() {
+    var twitterBase = "https://twitter.com/intent/tweet"
+    var shareText = '"' + document.getElementById("page-title").innerText + '" by @mr_rigden' 
+  
+    var shareLink = twitterBase + "?url=" + encodeURIComponent(shareURL) + "&text=" + encodeURIComponent(shareText) + "&via+" + "mr_rigden" 
+    var a = document.createElement('a')
+    a.innerHTML = 'Share';
+    a.setAttribute('href',shareLink)
+    document.getElementById("share").appendChild(a)
+  }
+
+
 if (navigator.share) {
-    console.log("new")
+    newShare()
 } else {
-    console.log("old")
+    twitterShare()
 }
